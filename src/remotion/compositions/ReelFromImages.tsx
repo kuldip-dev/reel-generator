82c1b1c04473d5454f268f5dfb0407556b571a10
kuldip-dev <kuldip2745.dev@gmail.com>
2026-05-26 18:44:25 +0530

    Update bottom padding in ReelFromImages component to 430 for improved layout consistency.

--- src/remotion/compositions/ReelFromImages.tsx
+++ src/remotion/compositions/ReelFromImages.tsx
@@ -565,7 +565,7 @@ function positionStyles(pos: TextPosition): React.CSSProperties {
     justifyContent: vAlign,
     alignItems: hAlign,
     paddingTop: isTop ? 100 : 0,
-    paddingBottom: isBottom ? 350 : 0,
+    paddingBottom: isBottom ? 430 : 0,
     paddingLeft: 60,
     paddingRight: 80,
     textAlign,
